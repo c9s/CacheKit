6b28ae8b873414925974c798ca150917f33a4e81
c9s <cornelius.howl@gmail.com>
2013-12-29 23:51:27 +0800

    Delete old bootstrap script

--- tests/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'tests/helpers.php';
-require 'Universal/ClassLoader/SplClassLoader.php';
-$loader = new \Universal\ClassLoader\SplClassLoader( array(  
-    'CacheKit' => 'src'
-));
-$loader->useIncludePath(true);
-$loader->register();
